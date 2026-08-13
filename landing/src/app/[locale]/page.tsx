bfad826c511df00838132dbbe2d717ce2dd4af3f
ihor-df <ihor.l@df.solutions>
2026-08-13 18:47:01 +0300

    Create sanity data fetching

--- landing/src/app/[locale]/page.tsx
+++ landing/src/app/[locale]/page.tsx
@@ -1,11 +1,34 @@
 import Container from "@components/atoms/container";
 import Image from "next/image";
+import { type SanityDocument } from "next-sanity";
+import { client } from "@/sanity/client";
+import { Link } from "@/i18n/navigation";
 
-export default function Home() {
+export default async function Home() {
+
+const POSTS_QUERY = `*[
+  _type == "post"
+  && defined(slug.current)
+]|order(publishedAt desc)[0...12]{_id, title, slug, publishedAt}`;
+
+const options = { next: { revalidate: 30 } };
+const posts = await client.fetch<SanityDocument[]>(POSTS_QUERY, {}, options);
+
   return (
     <Container>
       <main className="py-32">
         <Image className="h-5 w-25 dark:invert" src="/next.svg" alt="Next.js logo" width={100} height={20} priority />
+
+        <ul className="flex flex-col gap-y-4 mt-10">
+        {posts.map((post) => (
+          <li className="hover:underline" key={post._id}>
+            <Link href={`/${post.slug.current}`}>
+              <h2 className="text-xl font-semibold">{post.title}</h2>
+              <p>{new Date(post.publishedAt).toLocaleDateString()}</p>
+            </Link>
+          </li>
+        ))}
+      </ul>
       </main>
     </Container>
   );
---
+++ landing/src/sanity/client.ts
@@ -0,0 +1,10 @@
+import { createClient } from "next-sanity";
+
+const SANITY_API_VERSION = "2026-08-13"
+
+export const client = createClient({
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  apiVersion: SANITY_API_VERSION,
+  useCdn: false,
+});
