60b1ed699c49c571df74d1f622ca83e4480d89b9
ihor-df <ihor.l@df.solutions>
2026-08-13 14:35:07 +0300

    Add Search input

--- src/app/[locale]/ui/page.tsx
+++ src/app/[locale]/ui/page.tsx
@@ -1,5 +1,7 @@
 import GlassButton from "@/components/atoms/glass-button";
+import GlassInput from "@/components/atoms/glass-input";
 import Button from "@/components/atoms/main-button";
+import SearchInput from "@/components/atoms/search-input";
 import Container from "@components/atoms/container";
 import Image from "next/image";
 
@@ -10,6 +12,8 @@ export default function Page() {
         <Image className="h-5 w-25 dark:invert" src="/next.svg" alt="Next.js logo" width={100} height={20} priority />
 
         <div className="mt-20 flex flex-col gap-10">
+          <SearchInput />
+          <GlassInput />
           <GlassButton>Button</GlassButton>
           <Button header>Start for free</Button>
           <Button size="large">Button</Button>
@@ -18,6 +22,23 @@ export default function Page() {
           </Button>
           <Button>Start for free</Button>
           <Button variant="secondary">Start for free</Button>
+
+          <p className="text-2xl">Disabled</p>
+
+          <GlassButton disabled>Button</GlassButton>
+          <Button disabled header>
+            Start for free
+          </Button>
+          <Button disabled size="large">
+            Button
+          </Button>
+          <Button disabled size="large" variant="secondary">
+            Button
+          </Button>
+          <Button disabled>Start for free</Button>
+          <Button disabled variant="secondary">
+            Start for free
+          </Button>
         </div>
       </main>
     </Container>
---
+++ src/components/atoms/glass-input.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { ComponentPropsWithoutRef, useState } from "react";
+
+type GlassInputProps = Omit<
+  ComponentPropsWithoutRef<"input">,
+  "className" | "onChange" | "value"
+> & {
+  className?: string;
+};
+
+const GlassInput = ({
+  className,
+  onBlur,
+  onFocus,
+  ...props
+}: GlassInputProps) => {
+  const [focused, setFocused] = useState(false);
+  const [value, setValue] = useState("");
+
+  return (
+    <div
+      className={cn(
+        "glass group flex h-12 w-full max-w-80 items-center rounded-full px-5 hover:bg-white/20!",
+        focused && "bg-white/20!",
+        className,
+      )}
+    >
+      <input
+        {...props}
+        value={value}
+        className={cn(
+          "relative z-2 h-full w-full bg-transparent font-medium text-white placeholder-white/70 transition-colors outline-none group-hover:placeholder:text-white",
+          focused && "placeholder:text-white",
+        )}
+        onChange={(e) => setValue(e.target.value)}
+        onFocus={(event) => {
+          setFocused(true);
+          onFocus?.(event);
+        }}
+        onBlur={(event) => {
+          setFocused(false);
+          onBlur?.(event);
+        }}
+      />
+    </div>
+  );
+};
+
+export default GlassInput;
