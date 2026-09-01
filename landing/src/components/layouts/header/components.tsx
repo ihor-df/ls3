7dcd422d6c6a3ba689a5ff89a32c3e139f922cf8
ihor-df <ihor.l@df.solutions>
2026-09-01 11:33:22 +0300

    Update components.tsx

--- landing/src/components/layouts/header/components.tsx
+++ landing/src/components/layouts/header/components.tsx
@@ -15,7 +15,7 @@ const SecondLevelMenuItem = ({ href, icon, label, className, active }: SecondLev
   return (
     <li
       className={cn(
-        "group rounded-full transition-colors duration-300 hover:bg-white/10",
+        "rounded-small transition-colors duration-300 hover:bg-white/10",
         active && "bg-white/10",
         className,
       )}
@@ -23,8 +23,7 @@ const SecondLevelMenuItem = ({ href, icon, label, className, active }: SecondLev
       <Link className="flex max-w-52 items-center gap-4 p-3 leading-[1.1] tracking-[-0.01em]" href={href}>
         <span
           className={cn(
-            "flex size-10 min-w-10 items-center justify-center rounded-lg bg-white/10 transition-all duration-100 group-hover:rounded-full",
-            active && "rounded-full",
+            "flex size-10 min-w-10 items-center justify-center rounded-lg bg-white/10 transition-all duration-100",
           )}
         >
           <Icon className="size-5" />
